1c0edbfa0f905f949881dd18afa2ab4d79c3f667
Onkar-Kolhe <66004784+Onkar-Kolhe@users.noreply.github.com>
2020-05-27 14:39:47 +0530

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,339 @@
+class fox():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Black"
+		self.furcolour = "Orange"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Fox has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Fox's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Fox's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of fox is 'ow-wow-wow-wow'")
+	def run(self):
+		pass
+
+print("1) Fox")
+c = fox()
+c.set__legs(3)
+c.get__legs()
+c.set__eyecolour("Blue")
+c.get__eyecolour()
+c.set__furcolour("Brown")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class tiger():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Black"
+		self.furcolour = "Orange and Black strips"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Tiger has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Tiger's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Tiger's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of tiger is 'roar'")
+	def run(self):
+		pass
+
+print("2) Tiger")
+c = tiger()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Yellow")
+c.get__eyecolour()
+c.set__furcolour("White")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class lion():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Green"
+		self.furcolour = "Deep Orange"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Lion has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Lion's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Lion's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of lion is 'roar'")
+	def run(self):
+		pass
+
+print("3) Lion")
+c = lion()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Orange")
+c.get__eyecolour()
+c.set__furcolour("Brown")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class dog():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Black"
+		self.furcolour = "Deep Orange"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Dog has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Dog's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Dog's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of dog is 'Bark'")
+	def run(self):
+		pass
+
+print("4) Dog")
+c = dog()
+c.set__legs(3)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("White")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class horse():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Deep Orange"
+		self.furcolour = "White"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Horse has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Horse's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Horse's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of Horse is 'Whinny'")
+	def run(self):
+		pass
+
+print("5) Horse")
+c = horse()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("Deep Orange")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class cat():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Green"
+		self.furcolour = "Black"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Cat has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Cat's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Cat's fur colour is "+str(self.furcolour))
+	def speak(self):
+		print("Sound of cat is 'Meow'")
+	def run(self):
+		pass
+
+print("6) Cat")
+c = cat()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("White")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class Bull():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Gray"
+		self.furcolour = "Black"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Bull has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Bull's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Bull's fur colour is "+str(self.furcolour))
+	def speak(self):
+		pass
+	def run(self):
+		pass
+
+print("7) Bull")
+c = Bull()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("Deep orange")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class Cow():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Gray"
+		self.furcolour = "Black"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Cow has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Cow's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Cow's fur colour is "+str(self.furcolour))
+	def speak(self):
+		pass
+	def run(self):
+		pass
+
+print("8) Cow")
+c = Cow()
+c.set__legs(5)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("Deep orange")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class goat():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Gray"
+		self.furcolour = "Black"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Goat has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Goat's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Goat's fur colour is "+str(self.furcolour))
+	def speak(self):
+		pass
+	def run(self):
+		pass
+
+print("9) Goat")
+c = goat()
+c.set__legs(4)
+c.get__legs()
+c.set__eyecolour("Black")
+c.get__eyecolour()
+c.set__furcolour("White")
+c.get__furcolour()
+c.speak()
+c.run()
+
+class Hyena():
+	def init(self):
+		self.legs = 4
+		self.eyecolour = "Black"
+		self.furcolour = "Black"
+		self.type = "Mammal"
+	def set__legs(self,n):
+		self.legs = n
+	def get__legs(self):
+		print("Hyena has "+str(self.legs)+" legs")
+	def set__eyecolour(self,colour):
+		self.eyecolour = colour
+	def get__eyecolour(self):
+		print("Hyena's eyecolour is " +str(self.eyecolour))
+	def set__furcolour(self,colour):
+		self.furcolour = colour
+	def get__furcolour(self):
+		print("Hyena's fur colour is "+str(self.furcolour))
+	def speak(self):
+		pass
+	def run(self):
+		pass
+
+print("10) Hyena")
+c = Hyena()
+c.set__legs(3)
+c.get__legs()
+c.set__eyecolour("Gray")
+c.get__eyecolour()
+c.set__furcolour("Orange")
+c.get__furcolour()
+c.speak()
+c.run()
